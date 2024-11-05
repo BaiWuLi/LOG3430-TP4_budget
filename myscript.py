14decdfeb4eaaac3f35508f52c8d68f2096b2c6d
BaiWuLi <libaiwu59678@gmail.com>
2024-11-05 15:29:07 -0500

    add myscript.py

---
+++ myscript.py
@@ -0,0 +1,8 @@
+import os
+
+badhash = "c1a4be04b972b6c17db242fc37752ad517c29402"
+goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+command = "python manage.py test"
+
+os.system(f"git bisect start {badhash} {goodhash}")
+os.system(f"git bisect run {command}")
